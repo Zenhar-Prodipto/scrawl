6cfbef077c3275b3c17aeb16048631ee02f77137
Zenhar <zenhar.mz@gmail.com>
2025-03-01 23:58:03 +0600

    feat: Add custom User model with Interest table and admin setup

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, Interest
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'get_interests')
+    search_fields = ('username', 'email')
+
+    def get_interests(self, obj):
+        return ", ".join([interest.name for interest in obj.interests.all()])
+    get_interests.short_description = "Interests"
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)

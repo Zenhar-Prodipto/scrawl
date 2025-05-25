87b87159830c9ad2bb07844d5cb04cf4b61a724c
Zenhar <zenhar.mz@gmail.com>
2025-05-25 22:32:07 +0600

    clean: remove comments from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
-# This script is used to set up the environment for the Django application
-# and wait for PostgreSQL to be ready before running migrations and starting the server.
-# It is executed when the Docker container starts.
-# Make sure to give execute permissions to this script
-# before building the Docker image.
-# Usage: chmod +x entrypoint.sh
-
 #!/bin/sh
 
 # Print the environment variables to verify if they are being set properly

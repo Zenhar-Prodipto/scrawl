be5df074b808fc425e98ef756ebc5896c4fe6725
Zenhar <zenhar.mz@gmail.com>
2025-06-21 22:13:26 +0600

    chore: integrate redis

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Print the environment variables to verify if they are being set properly
 echo "DB_HOST: $DB_HOST"
 echo "DB_PORT: $DB_PORT"
+echo "REDIS_URL: $REDIS_URL"
 
 echo "Waiting for PostgreSQL to start..."
 
@@ -12,6 +13,15 @@ while ! nc -z $DB_HOST $DB_PORT; do
 done
 
 echo "PostgreSQL started!"
+
+# Wait until Redis is ready (parse REDIS_URL for host and port)
+REDIS_HOST=$(echo $REDIS_URL | sed 's|redis://\(.*\):[0-9]*/[0-9]*|\1|')
+REDIS_PORT=$(echo $REDIS_URL | sed 's|redis://[^:]*:\([0-9]*\)/[0-9]*|\1|')
+while ! nc -z $REDIS_HOST $REDIS_PORT; do
+  sleep 1
+done
+
+echo "Redis started!"
 
 # Run migrations
 python manage.py migrate

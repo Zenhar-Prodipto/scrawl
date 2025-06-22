aa02753e1a707044e8837ced55df1fbec560089c
Zenhar <zenhar.mz@gmail.com>
2025-06-22 22:32:03 +0600

    chore: integrate kafka

--- scrawl/settings.py
+++ scrawl/settings.py
@@ -107,6 +107,24 @@ DATABASES = {
     
 }
 
+# Redis Cache Configuration
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),  # Fallback if .env missing
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Kafka Configuration
+KAFKA = {
+    'BOOTSTRAP_SERVERS': os.getenv('KAFKA_BROKER', 'drf_scrawl_kafka:9092'),
+    'SECURITY_PROTOCOL': 'PLAINTEXT',
+    'API_VERSION': (2, 3, 0)
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
